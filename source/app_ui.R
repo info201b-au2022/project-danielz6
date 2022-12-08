4c7948a3dae2b0f6cff2c6ee0bc2dfdd727855b8
EddyW206 <eddyw206@hotmail.com>
2022-12-08 10:52:45 -0800

    Tab Panel Chart

--- source/app_ui.R
+++ source/app_ui.R
@@ -87,6 +87,12 @@ page1_panel <- tabPanel(
   # tab_panel_chart2,
   # tab_panel_chart3,
 
+tab_panel_chart1 <-tabPanel(
+  "Chart 1",
+  p("This bar chart analyzes the percentage of countries' population that has safely managed water access."),
+  selectInput("Year", "Select year:", choices = c("2015", "2016", "2017", "2018", "2019", "2020"), selected = "2020"),
+  plotOutput("plot_water_access_by_country")
+)
 
 # The project summary
 page2_sidebar_content <- sidebarPanel(
@@ -97,12 +103,6 @@ page2_sidebar_content <- sidebarPanel(
     p("2"),
     p("3")
   )  
-  tab_panel_chart1 <-tabPanel(
-    "Chart 1",
-    p("This bar chart analyzes the percentage of countries' population that has safely managed water access."),
-    selectInput("Year", "Select year:", choices = c("2015", "2016", "2017", "2018", "2019", "2020"), selected = "2020"),
-    plotOutput("plot_water_access_by_country")
-  )
   page2_panel <- tabPanel(
     "Key Takeaways", titlePanel("Title"),
     page_2__main_content
@@ -114,6 +114,7 @@ page2_sidebar_content <- sidebarPanel(
     page2_panel
 
  )
+  
 
 #   # The project report
 #   tab_panel_report
