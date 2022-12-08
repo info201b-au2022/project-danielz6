865bedbf74e22a68acf46aa3c854d7c5c580b582
EddyW206 <eddyw206@hotmail.com>
2022-12-08 10:49:08 -0800

    Chart 1 Tab

--- source/app_ui.R
+++ source/app_ui.R
@@ -97,6 +97,12 @@ page2_sidebar_content <- sidebarPanel(
     p("2"),
     p("3")
   )  
+  tab_panel_chart1 <-tabPanel(
+    "Chart 1",
+    p("This bar chart analyzes the percentage of countries' population that has safely managed water access."),
+    selectInput("Year", "Select year:", choices = c("2015", "2016", "2017", "2018", "2019", "2020"), selected = "2020"),
+    plotOutput("plot_water_access_by_country")
+  )
   page2_panel <- tabPanel(
     "Key Takeaways", titlePanel("Title"),
     page_2__main_content
