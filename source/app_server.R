34ed228d0f85487822f90cbb29b4a7771d59d6e9
EddyW206 <eddyw206@hotmail.com>
2022-12-08 12:32:28 -0800

    Finished Bar Chart

--- source/app_server.R
+++ source/app_server.R
@@ -16,17 +16,6 @@ wateraccessdata <- read.csv(data)
 
 x_values <- c("NZL", "DEU", "KOR", "FRA", "CAN", "JPN", "NOR", "USA", "HUN", "MNE", "PSE", "RUS", "COL", "ECU", "NIC", "PHL", "MEX", "PAK", "NGA", "ETH")
 
-plot_water_access_by_country <- function() {
-  bar_chart <- ggplot(water_access_by_country(Code), aes(x = Code, y = Safely_Managed_Water)) +
-    geom_bar(stat = "identity", fill = "lightblue", color = "black") +
-    scale_x_discrete(limits = x_values) +
-    labs(x = "Country Code", y = "Percentage of Safely Managed Water (2020)", title = "Safely Managed Water Access by Country") +
-    theme(panel.background = element_rect(fill = "gray"), panel.grid.major = element_line(color = "white"))
-  return(bar_chart)
-}
-
-plot_water_access_by_country()
-
 server <- function(input, output) {
   data_by_year <- reactive({
     wateraccessdata[wateraccessdata$Code %in% c('USA', "NOR", "PAK", "NGA", "NIC", "MNE", "RUS", "COL", "KOR", "PHL", "PSE", "MEX", "CAN", "FRA", "DEU", "JPN", "HUN", "ECU", "NZL", "ETH"), ] %>% 
